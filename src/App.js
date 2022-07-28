16d4073e7fb49057bdf7637b09569adb27ec37f4
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-28 21:53:24 +0200

    Added more files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { FaGithubSquare } from "react-icons";
-
 import Review from "./Review";
 
 function App() {
-  return <h1>Reviews</h1>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Review.js
+++ src/Review.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
 const Review = () => {
   return <div>Review</div>;
 };
